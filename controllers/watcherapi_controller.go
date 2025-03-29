0796e9659417afe78948cb370bf013e0d8f24cd8
Sean Mooney <work@seanmooney.info>
2025-03-29 21:55:32 +0000

    fix nil point deref in error path.
    while seting up my editor to highlight go
    i found a could of nill pointer derefs via static checking
    
    i have not reviewed all file but this commit fixes the 3 cases
    that i found today.

--- controllers/watcherapi_controller.go
+++ controllers/watcherapi_controller.go
@@ -304,8 +304,8 @@ func (r *WatcherAPIReconciler) Reconcile(ctx context.Context, req ctrl.Request) 
 			condition.ErrorReason,
 			condition.SeverityWarning,
 			condition.ServiceConfigReadyErrorMessage,
-			err.Error()))
-		return ctrl.Result{}, err
+			errorHash.Error()))
+		return ctrl.Result{}, errorHash
 	} else if hashChanged {
 		// Hash changed and instance status should be updated (which will be done by main defer func),
 		// so we need to return and reconcile again
